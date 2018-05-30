a10ceee76c20326906a7aac3077a43cef5d599b7
Balint Flaviu <gascaceltare@gmail.com>
2018-05-30 17:52:36 +0300

    Ba deci am bagat settings plugin

---
+++ Sleep_Tight_v1_3/Sleep_Tight_v1_3.Android/Settings.cs
@@ -0,0 +1,46 @@
+﻿// Helpers/Settings.cs  
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace QbizApp.Helpers
+{
+    /// <summary>  
+    /// This is the Settings static class that can be used in your Core solution or in any  
+    /// of your client applications. All settings are laid out the same exact way with getters  
+    /// and setters.   
+    /// </summary>  
+    public static class LocalSettings
+    {
+        static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        public static string Setting1
+        {
+            get => AppSettings.GetValueOrDefault(nameof(Setting1), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(Setting1), value);
+        }
+
+        public static bool ShowGDPR
+        {
+            get => AppSettings.GetValueOrDefault(nameof(ShowGDPR), false);
+            set => AppSettings.AddOrUpdateValue(nameof(ShowGDPR), value);
+        }
+
+        public static string ShowLogin
+        {
+            get => AppSettings.GetValueOrDefault(nameof(ShowLogin), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(ShowLogin), value);
+        }
+
+        public static void ClearAllData()
+        {
+            AppSettings.Clear();
+        }
+
+    }
+}
