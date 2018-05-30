c594e34050589a25da8df6cb2958c900f2d20f21
Armin Purle-Kopacz <pukoarmin@gmail.com>
2018-05-30 16:44:09 +0300

    initial commit with project

---
+++ Sleep_Tight_v1_3/Sleep_Tight_v1_3/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sleep_Tight_v1_3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Detail = new NavigationPage(new Start_Page());
+            IsPresented = false;
+        }
+
+        private void Handle_Clicked_Start_Page(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Start_Page());
+            IsPresented = false;
+        }
+
+        private void Handle_Clicked_Settings(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Settings());
+            IsPresented = false;
+        }
+
+        private void Handle_Clicked_Information(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Information());
+            IsPresented = false;
+        }
+
+        private void Handle_Clicked_Tips(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Tips());
+            IsPresented = false;
+        }
+
+        private void Handle_Clicked_About(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new About());
+            IsPresented = false;
+        }
+    }
+}
