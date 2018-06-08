b12f22854f1d467186d6836348c6b33ad8e081fa
Armin Purle-Kopacz <pukoarmin@gmail.com>
2018-06-08 17:01:41 +0300

    SegmentedSleep Algorithm

--- Sleep_Tight_v1_3/Sleep_Tight_v1_3/Pages/Start_Page.xaml.cs
+++ Sleep_Tight_v1_3/Sleep_Tight_v1_3/Pages/Start_Page.xaml.cs
@@ -22,5 +22,22 @@ namespace Sleep_Tight_v1_3
         {
             test.Text = TimePickerGoSleep.Time.Hours.ToString();
         }
+
+        private void SegmentedSleep()
+        {
+            TimeSpan StartTime, WakeTime, SleepTime;
+            //wakeup call
+            StartTime = TimePickerGoSleep.Time;
+            SleepTime = new TimeSpan(3, 30, 0);
+            WakeTime = StartTime + SleepTime;
+            
+            //aici il pune la nani iara
+            SleepTime = new TimeSpan(2, 0, 0);
+            WakeTime = WakeTime + SleepTime;
+
+            //second wakeup call
+            SleepTime = new TimeSpan(3, 30, 0);
+            WakeTime = WakeTime + SleepTime;
+        }
     }
 }
