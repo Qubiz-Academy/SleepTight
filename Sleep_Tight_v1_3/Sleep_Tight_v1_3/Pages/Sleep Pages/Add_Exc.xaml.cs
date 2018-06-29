37a2a6b190c5f35b818b1e52ea7da61cc2e31945
Armin Purle-Kopacz <pukoarmin@gmail.com>
2018-06-29 15:27:55 +0300

    sync error

--- Sleep_Tight_v1_3/Sleep_Tight_v1_3/Pages/Sleep Pages/Add_Exc.xaml.cs
+++ Sleep_Tight_v1_3/Sleep_Tight_v1_3/Pages/Sleep Pages/Add_Exc.xaml.cs
@@ -16,5 +16,39 @@ namespace Sleep_Tight_v1_3.Pages.Sleep_Pages
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void TimePickerStartExc_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            TimeSpan StartExc;
+            StartExc = TimePickerStartExc.Time;
+            App.StartExc = StartExc;
+        }
+
+        private void TimePickerEndExc_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            TimeSpan EndExc;
+            EndExc = TimePickerStartExc.Time;
+            App.EndExc = EndExc;
+        }
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            //se populeaza list view
+            ExcList.ItemsSource = new Models.ExcInterval[] {
+                new Models.ExcInterval {
+                    StartExc = new TimeSpan(14, 13, 12),
+                    EndExc = new TimeSpan(13, 12, 21)
+                }
+            };
+            //se verifica daca influenteaza
+            //daca se poate si inainte si dupa muta intervalul
+            //se intreaba daca vrea sa fie mutat intervalul inainte sau dupa exceptie
+            //se modifica variabilele
+        }
+
+        private void Rec_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
